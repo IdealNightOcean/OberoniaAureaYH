ebdcc9552d73a7705fae37b842d1c1f1085af2e3
“IdealNightOcean” <idealyehai@qq.com>
2024-06-23 18:55:21 +0800

    温度基因恢复

--- OberoniaAureaYH/Thought/ThoughtWorker_Gene_ColdAdaptation.cs
+++ OberoniaAureaYH/Thought/ThoughtWorker_Gene_ColdAdaptation.cs
@@ -7,15 +7,20 @@ public class ThoughtWorker_Gene_ColdAdaptation : ThoughtWorker
 {
     protected override ThoughtState CurrentStateInternal(Pawn p)
     {
-        if (!ModsConfig.BiotechActive)
+        if (!ModsConfig.BiotechActive || p.genes == null)
         {
             return ThoughtState.Inactive;
         }
-        float ambientTemperature = p.AmbientTemperature;
-        if (ambientTemperature < -15f)
+
+        if (p.genes.HasGene(OA_PawnInfoDefOf.OA_RK_Gene_ColdAdaptation))
         {
-            return ThoughtState.ActiveDefault;
+            float ambientTemperature = p.AmbientTemperature;
+            if (ambientTemperature < -15f)
+            {
+                return ThoughtState.ActiveDefault;
+            }
         }
+
         return ThoughtState.Inactive;
     }
 }
--- OberoniaAureaYH_1.4/Thought/ThoughtWorker_Gene_HotAdaptation.cs
+++ OberoniaAureaYH_1.4/Thought/ThoughtWorker_Gene_HotAdaptation.cs
@@ -7,15 +7,20 @@ public class ThoughtWorker_Gene_HotAdaptation : ThoughtWorker
 {
     protected override ThoughtState CurrentStateInternal(Pawn p)
     {
-        if (!ModsConfig.BiotechActive)
+        if (!ModsConfig.BiotechActive || p.genes == null)
         {
             return ThoughtState.Inactive;
         }
-        float ambientTemperature = p.AmbientTemperature;
-        if (ambientTemperature > 45f)
+
+        if (p.genes.HasGene(OA_PawnInfoDefOf.OA_RK_Gene_HotAdaptation))
         {
-            return ThoughtState.ActiveDefault;
+            float ambientTemperature = p.AmbientTemperature;
+            if (ambientTemperature > 45f)
+            {
+                return ThoughtState.ActiveDefault;
+            }
         }
+
         return ThoughtState.Inactive;
     }
 }
