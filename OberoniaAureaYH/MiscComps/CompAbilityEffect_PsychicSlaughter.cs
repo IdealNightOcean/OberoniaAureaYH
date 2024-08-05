c2d163e1c82faac12473b5fcc6537ce9830e5b74
“IdealNightOcean” <idealyehai@qq.com>
2024-08-05 12:29:23 +0800

    心灵宰杀后果调整

--- OberoniaAureaYH/MiscComps/CompAbilityEffect_PsychicSlaughter.cs
+++ OberoniaAureaYH/MiscComps/CompAbilityEffect_PsychicSlaughter.cs
@@ -16,24 +16,14 @@ public class CompAbilityEffect_PsychicSlaughter : RimWorld.CompAbilityEffect_Psy
         Faction oaFaction = OberoniaAureaYHUtility.OAFaction;
         foreach (LocalTargetInfo localTarget in targets)
         {
-            if (localTarget.HasThing && localTarget.Thing is Pawn pawn)
+            if (localTarget.HasThing && localTarget.Thing is Pawn)
             {
                 Faction.OfPlayer.TryAffectGoodwillWith(oaFaction, -200, canSendMessage: false, canSendHostilityLetter: true, OA_HistoryEventDefOf.OA_PsychicSlaughter);
                 if (Faction.OfPlayer.RelationKindWith(oaFaction) != FactionRelationKind.Hostile)
                 {
                     Faction.OfPlayer.SetRelationDirect(oaFaction, FactionRelationKind.Ally, canSendHostilityLetter: true, reason: "OA_PsychicSlaughter");
                 }
-                if (pawn.RaceProps.Humanlike && pawn.Faction == oaFaction)
-                {
-
-                    GameComponent_OberoniaAurea oa_GCOA = OberoniaAureaYHUtility.OA_GCOA;
-                    if (oa_GCOA != null)
-                    {
-                        oa_GCOA.assistPointsStoppageDays = 60;
-                        oa_GCOA.UseAssistPoints(99999);
-                    }
-                    break;
-                }
+                break;
             }
         }
     }
