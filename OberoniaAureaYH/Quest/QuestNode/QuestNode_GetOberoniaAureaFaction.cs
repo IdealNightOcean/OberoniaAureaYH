a8febb85e1c2a4012c411eb63f7c2133d1ed597c
“IdealNightOcean” <idealyehai@qq.com>
2024-08-21 18:23:43 +0800

    获取金鼠鼠派系QuestNode修复

--- OberoniaAureaYH/Quest/QuestNode/QuestNode_GetOberoniaAureaFaction.cs
+++ OberoniaAureaYH/Quest/QuestNode/QuestNode_GetOberoniaAureaFaction.cs
@@ -16,25 +16,34 @@ public class QuestNode_GetOberoniaAureaFaction : QuestNode
 
     protected override void RunInt()
     {
-        Slate slate = QuestGen.slate;
-        if (SetVars(slate))
-        {
-            slate.Set(storeAs.GetValue(slate), OberoniaAureaYHUtility.OAFaction);
-        }
+        SetVars(QuestGen.slate);
     }
 
     protected override bool TestRunInt(Slate slate)
     {
         return SetVars(slate);
     }
-    private bool SetVars(Slate slate)
+
+    protected bool SetVars(Slate slate)
     {
         Faction OAFaction = OberoniaAureaYHUtility.OAFaction;
-        if (OAFaction == null)
+        if(ValidFaction(OAFaction,slate))
+        {
+            slate.Set(storeAs.GetValue(slate), OberoniaAureaYHUtility.OAFaction);
+            return true;
+        }
+        else
         {
             return false;
         }
-        FactionRelationKind playerRelationKind = OAFaction.PlayerRelationKind;
+    }
+    protected bool ValidFaction(Faction faction, Slate slate)
+    {
+        if (faction == null)
+        {
+            return false;
+        }
+        FactionRelationKind playerRelationKind = faction.PlayerRelationKind;
         return playerRelationKind switch
         {
             FactionRelationKind.Ally => allowAlly.GetValue(slate),
