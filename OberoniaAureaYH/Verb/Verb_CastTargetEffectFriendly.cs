a11ea6a0a3922aa68f6ac758ad7a422d29793df4
IdealNightOcean <idealyehai@qq.com>
2024-12-20 22:01:27 +0800

    verb集中

---
+++ OberoniaAureaYH/Verb/Verb_CastTargetEffectFriendly.cs
@@ -0,0 +1,50 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace OberoniaAurea;
+
+public class Verb_CastTargetEffectFriendly : Verb_CastTargetEffect
+{
+    public override void OnGUI(LocalTargetInfo target)
+    {
+        if (CanHitTarget(target) && IsFriendlyPawn(target))
+        {
+            if (verbProps.targetParams.CanTarget(target.ToTargetInfo(caster.Map)))
+            {
+                base.OnGUI(target);
+            }
+            else
+            {
+                GenUI.DrawMouseAttachment(TexCommand.CannotShoot);
+                if (!verbProps.invalidTargetPawn.NullOrEmpty())
+                {
+                    Widgets.MouseAttachedLabel(verbProps.invalidTargetPawn, 0f, -20f);
+                }
+            }
+        }
+        else
+        {
+            GenUI.DrawMouseAttachment(TexCommand.CannotShoot);
+        }
+    }
+
+    public override bool ValidateTarget(LocalTargetInfo target, bool showMessages = true)
+    {
+        if (!IsFriendlyPawn(target))
+        {
+            return false;
+        }
+        return base.ValidateTarget(target, showMessages);
+    }
+
+    private static bool IsFriendlyPawn(LocalTargetInfo target)
+    {
+        Pawn pawn = target.Pawn;
+        if (pawn == null)
+        {
+            return false;
+        }
+        return pawn.Faction == Faction.OfPlayer || pawn.Downed || pawn.IsPrisonerOfColony || pawn.IsQuestLodger();
+    }
+}
---
+++ OberoniaAureaYH/Verb/Verb_SummonEMP.cs
@@ -0,0 +1,41 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace OberoniaAurea;
+
+public class Verb_SummonEMP : Verb
+{
+    public override void OrderForceTarget(LocalTargetInfo target)
+    {
+        Job job = JobMaker.MakeJob(JobDefOf.UseVerbOnThingStatic, target);
+        job.verbToUse = this;
+        CasterPawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
+    }
+
+    protected override bool TryCastShot()
+    {
+        if (currentTarget.HasThing && currentTarget.Thing.Map != caster.Map)
+        {
+            return false;
+        }
+        CompApparelReloadable reloadableCompSource = base.ReloadableCompSource;
+        if (reloadableCompSource == null)
+        {
+            TryShot();
+            return true;
+        }
+        else if (reloadableCompSource.CanBeUsed(out var _))
+        {
+            TryShot();
+            reloadableCompSource.UsedOnce();
+            return true;
+        }
+        return false;
+    }
+    protected void TryShot()
+    {
+        Thing thing = ThingMaker.MakeThing(verbProps.spawnDef);
+        DropPodUtility.DropThingsNear(currentTarget.Cell, caster.Map, [thing], 60, faction: caster.Faction);
+    }
+}
